f1e6a487eb46388911b6d1596cce6f278f5e89a2
pfunkmallone <eric.dexter@kbslp.com>
2020-07-01 16:04:11 +0000

    supress output

--- containers101/understanding-images/foreground.sh
+++ containers101/understanding-images/foreground.sh
@@ -1,2 +1,4 @@
-[ ! -x /usr/bin/docker ] || echo "You got a bum Katacoda session...you'll need to refresh this page and try again"
-sleep 3; wait.sh 2>&1 >/dev/null
+{
+	[ ! -x /usr/bin/docker ] && echo "You got a bum Katacoda session...you'll need to refresh this page and try again"
+sleep 3; wait.sh
+} 2> /dev/null
