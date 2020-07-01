13e6c86500c8cd0bd6c21609913a82f8111ac224
pfunkmallone <pfunkbulk@gmail.com>
2020-07-01 23:18:22 +0000

    move to nodejs directory

--- containers101/understanding-persistentstorage/background.sh
+++ containers101/understanding-persistentstorage/background.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /root
 git clone https://github.com/niinpatel/nodejs-image-upload.git
+mv Dockerfile nodejs-image-upload/
+cd nodejs-image-upload/
 echo "done" >> /root/katacoda-finished
 docker build -t nodeapp:0.1 .
 echo "done" >> /root/katacoda-background-finished
