9a090a1c53ea83436543f390723713038f9dda07
pfunkmallone <eric.dexter@kbslp.com>
2020-07-01 17:23:21 +0000

    sleep 3

--- containers101/your-first-container/foreground.sh
+++ containers101/your-first-container/foreground.sh
@@ -1,1 +1,1 @@
-[ ! -x /usr/bin/docker ] && echo "Docker not installed...You have a broken Katacoda environment. Please refresh this webpage"
+sleep 3; [ ! -x /usr/bin/docker ] && echo "Docker not installed...You have a broken Katacoda environment. Please refresh this webpage"
