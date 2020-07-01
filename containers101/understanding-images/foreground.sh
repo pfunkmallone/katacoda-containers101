0dc79298f7a55e2e7818efa62fa9c6d347e76c53
pfunkmallone <eric.dexter@kbslp.com>
2020-07-01 16:07:20 +0000

    well, back to the books

--- containers101/understanding-images/foreground.sh
+++ containers101/understanding-images/foreground.sh
@@ -1,4 +1,2 @@
-{
-	[ ! -x /usr/bin/docker ] && echo "You got a bum Katacoda session...you'll need to refresh this page and try again"
+[ ! -x /usr/bin/docker ] && echo "You got a bum Katacoda session...you'll need to refresh this page and try again"; exit
 sleep 3; wait.sh
-} 2> /dev/null
